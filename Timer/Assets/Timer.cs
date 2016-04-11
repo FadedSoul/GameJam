8a1b574aa718ccf0f708d50993dd2022623139e1
JonathanKievits <Jonathan.kievits@hotmail.nl>
2016-04-11 16:17:32 +0200

    timer

---
+++ Timer/Assets/Timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Text uiText;
+    private float time;
+
+
+    void Start () {
+        time = 0f;
+	}
+	
+	void Update () {
+	    if (uiText != null)
+        {
+            uiText.text = "Time: " + time.ToString("f0");
+        }
+        time += Time.deltaTime;
+	}
+}
