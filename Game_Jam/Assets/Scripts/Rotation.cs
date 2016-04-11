2b243ba1f88d055e18c134b1dadf7b3bcb51789b
Niels <Niels97vet@hotmail.com>
2016-04-11 12:02:27 +0200

    Niels(Rotate)

---
+++ Game_Jam/Assets/Scripts/Rotation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    private float Speed;
+	// Use this for initialization
+	void Start () {
+        Speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, Time.deltaTime * Speed);
+
+        if(Speed > 10)
+        {
+            if (Input.GetKey(KeyCode.A))
+                Speed -= 1f;
+        }
+
+        if (Speed < 100)
+        {
+            if (Input.GetKey(KeyCode.D))
+                Speed += 1f;
+        }
+	}
+}
