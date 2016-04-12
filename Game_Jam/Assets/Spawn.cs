afe306c46728193e8737a3c065912a317b788071
MrElektronix <milovanpelt@gmail.com>
2016-04-12 13:38:00 +0100

    Spawning
    - need to add random spawning

---
+++ Game_Jam/Assets/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	private GameObject rondje;
+	public GameObject[] items;
+
+	// Use this for initialization
+	void Start () {
+		//rondje = Resources.Load<GameObject> ("Rondje_1");
+		//Instantiate (rondje, new Vector2 (0, 0), rondje.transform.rotation);
+		for (int i = 0; i < items.Length; i++) {
+			Instantiate (items[i], new Vector2 (i, i), items[i].transform.rotation);
+		
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
